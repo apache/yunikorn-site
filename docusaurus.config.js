dbc299eb760d1f98844b8a43446068f7d2aeb9ac
Weiwei Yang <wyang@cloudera.com>
2020-07-16 05:05:11 -0700

    [YUNIKORN-242] Fix some style issues in dark mode.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -133,14 +133,12 @@ module.exports = {
     },
     footer: {
       style: 'grey',
-      copyright: `
-
-        <img src="https://incubator.apache.org/images/incubator_feather_egg_logo_sm.png" style="max-width: 300px"> <br/>
-
-        Copyright © 2020 The Apache Software Foundation. <br/>
-        Apache and the Apache feather logo are trademarks of The Apache Software Foundation<br/> <br/>
-
-        <p style="text-align: left">Disclaimer: Apache YuniKorn (Incubating) is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.</p>`
+      logo: {
+        alt: 'Apache Incubator Logo',
+        src: 'https://incubator.apache.org/images/incubator_feather_egg_logo.png',
+        href: 'https://incubator.apache.org/',
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} The Apache Software Foundation. Apache and the Apache feather logo are trademarks of The Apache Software Foundation`
     },
   }
 };
