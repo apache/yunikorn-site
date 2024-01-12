4c7b20f78fba318a0b4985dee006d300f81d36e9
Yu-Lin Chen <kh87313@gmail.com>
2024-01-12 21:27:54 +0800

    [YUNIKORN-2274] Improve document version routing in YuniKorn documentation (#384)
    Closes: #384
    
    Signed-off-by: Chia-Ping Tsai <chia7712@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ module.exports = {
       items: [
         {
           to: 'docs/',
-          label: 'Quick Start',
+          label: 'Docs',
         },
         {
           to: 'community/roadmap',
@@ -154,24 +154,8 @@ module.exports = {
         },
         {
           label: 'Docs',
-          to: 'docs',
+          type: 'docsVersionDropdown',
           position: 'right',
-          items: [
-            {
-              label: 'Master',
-              to: 'docs/next/',
-            },
-            {
-              label: versions[0],
-              to: 'docs/',
-              // required for correct style on current version menu item
-              activeBaseRegex: `docs/(?!${versions.join('|')}|next)`,
-            },
-            ...versions.slice(1).map((version) => ({
-              label: version,
-              to: `docs/${version}/`,
-            })),
-          ],
         },
         {
           type: 'localeDropdown',
